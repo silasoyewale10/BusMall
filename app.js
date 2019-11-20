ae7cfdb725c9d93cb54cf60e955d8408801b4763
Silas Oyewale <silasoyewale10@gmail.com>
2019-11-19 23:27:39 -0800

    almost done

--- app.js
+++ app.js
@@ -1,202 +1,3 @@
-// function Product(name, imageSrc){
-//     this.name = name;
-//     this.clicks =0;
-//     this.timesShown =0;
-//     this.imageSrc = imageSrc;
-//     Product.arrOfProducts.push(this);
-// }
-// Product.arrOfProducts = [];
-// new Product('bag', './assets/bag.jpg');
-// new Product('banana', './assets/banana.jpg');
-// new Product('bathroom', './assets/bathroom.jpg');
-// new Product('boots', './assets/boots.jpg');
-// new Product('breakfast', './assets/breakfast.jpg');
-// new Product('bubblegum', './assets/bubblegum.jpg');
-// new Product('chair', './assets/chair.jpg');
-// new Product('cthulhu', './assets/cthulhu.jpg');
-// new Product('duck', './assets/dog-duck.jpg');
-// new Product('dragon', './assets/dragon.jpg');
-// new Product('pen', './assets/pen.jpg');
-// new Product('pet-sweep', './assets/pet-sweep.jpg');
-// new Product('scissors', './assets/scissors.jpg');
-// new Product('shark', './assets/shark.jpg');
-// new Product('sweep', './assets/sweep.png');
-// new Product('tantaun', './assets/tauntaun.jpg');
-// new Product('unicorn', './assets/unicorn.jpg');
-// new Product('usb', './assets/usb.gif');
-// new Product('water-can', './assets/water-can.jpg');
-// new Product('glass', './assets/wine-glass.jpg');
-
-
-// // console.log(Product().this.imageSrc)
-
-// // console.log(Product.arrOfProducts.length )
-// // console.log(Product.arrOfProducts[6].name)
-
-// var leftProductElem = document.getElementById("left_product_img");
-// var middleProductElem = document.getElementById("middle_product_img");
-// var rightProductElem = document.getElementById("right_product_img");
-
-// var left = document.createElement('left');
-// var middle = document.createElement('middle');
-// var right = document.createElement('right');
-
-// // leftProductElem.appendChild(left);
-// // middleProductElem.appendChild(middle);
-// // rightProductElem.appendChild(right);
-
-
-
-// // console.log('here',leftProductElem)
-
-
-// // var leftImage ;
-// // var middleImage = '';
-// // var rightImage ='';
-// var nameOfLeftImage;
-// var nameOfMiddleImage;
-// var nameOfRightImage;
-
-// // var arrImageStorer = [];
-
-// var arrImageStorer = [1,2,3];
-// var pickThreeProducts = function (leftImage, middleImage, rightImage) {
-//         var leftImageIndex = Math.floor(Math.random() * 20) ;
-//         leftProductElem.setAttribute('src',Product.arrOfProducts[leftImageIndex].imageSrc)
-//         nameOfLeftImage= Product.arrOfProducts[leftImageIndex].name;
-//         arrImageStorer.push(nameOfLeftImage);
-//         console.log(arrImageStorer);
-//         // left.textContent = arrImageStorer[0]
-
-
-
-//         var middleImageIndex = Math.floor(Math.random() * 20) ;
-//         middleProductElem.setAttribute('src',Product.arrOfProducts[middleImageIndex].imageSrc)
-//         nameOfMiddleImage = Product.arrOfProducts[middleImageIndex].name;
-//         arrImageStorer.push(nameOfMiddleImage);
-//         console.log(arrImageStorer);
-
-//         // middle.textContent =arrImageStorer[1]
-
-
-
-//         var rightImageIndex = Math.floor(Math.random() * 20) ;
-//         rightProductElem.setAttribute('src',Product.arrOfProducts[rightImageIndex].imageSrc)
-//         nameOfRightImage = Product.arrOfProducts[rightImageIndex].name;
-//         arrImageStorer.push(nameOfRightImage)
-//         console.log(arrImageStorer);
-
-//         // right.textContent =arrImageStorer[2]
-
-//         // console.log(arrImageStorer)
-//         arrImageStorer.pop()
-//         arrImageStorer.pop()
-//         arrImageStorer.pop()
-//     }
-
-
-//     pickThreeProducts();
-
-
-
-// var clickCounter=0;
-// var clickMaxDefault = 25;  //default max if no click count is inputted. 
-// var clickMax = 0;   
-// var choice = window.prompt("Boss, How many clicks do you want the user to make ? If you enter nothing, your default click max is 25")
-// if (choice.length == '0'){  //input is automatically a string in JS
-//     clickMax = clickMaxDefault;
-//     // console.log("click max " , clickMax)
-
-// }else {
-//     clickMax = choice;
-//     // console.log("click max " , clickMax)
-
-//  }
-// // console.log("")
-// leftProductElem.addEventListener('click', clickHandler)
-// middleProductElem.addEventListener('click', clickHandler)
-// rightProductElem.addEventListener('click', clickHandler)
-
-// function terminateEvent (){
-
-//     leftProductElem.removeEventListener('click', clickHandler)
-//     middleProductElem.removeEventListener('click', clickHandler)
-//     rightProductElem.removeEventListener('click', clickHandler)
-//     // console.log("you are done son")
-//     alert("You have made " +clickCounter + " clicks. No more for you")
-// }
-
-// function clickHandler(event){
-//     // for (var x =0; x <= 25; x++){
-//         pickThreeProducts();
-
-//         var imageleft = nameOfLeftImage;
-//     var imagemiddle = nameOfMiddleImage;
-//     var imageright = nameOfRightImage;
-
-
-// console.log(arrImageStorer)
-//     if(event.target.id == 'left_product_img'){
-//         nameOfLeftImage = arrImageStorer[0];
-//         // console.log("nameOfSelectedImage is " + arrImageStorer[0])
-//     }
-//         clickCounter++;
-//         if(clickCounter == clickMax){
-//             terminateEvent();
-//         }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function Product(name, imageSrc) {
     this.name = name;
     this.clicks = 0;
@@ -249,28 +50,40 @@ var nameOfRightImage;
 var leftImageIndex;
 var middleImageIndex;
 var rightImageIndex;
+
+var arrOfTimesShown =[];
+var arrOfClicks = [];
 var pickThreeProducts = function (leftImage, middleImage, rightImage) {
+    // arr[1]=2
     leftImageIndex = Math.floor(Math.random() * 20);
     var leftProductInstance = Product.arrOfProducts[leftImageIndex];
     leftProductElem.setAttribute('src', leftProductInstance.imageSrc)
     nameOfLeftImage = leftProductInstance.name;
     leftProductInstance.timesShown++
-    // console.log(this.name)
+
+    arrOfTimesShown.push(leftProductInstance.timesShown++)
+
+
+
     middleImageIndex = Math.floor(Math.random() * 20);
     var middleProductInstance = Product.arrOfProducts[middleImageIndex];
     middleProductElem.setAttribute('src', middleProductInstance.imageSrc)
     nameOfMiddleImage = middleProductInstance.name;
     middleProductInstance.timesShown++
 
-    // console.log('nameOfMiddleImage is ' + nameOfMiddleImage);
-    //console.log(this.name)
+    arrOfTimesShown.push(middleProductInstance.timesShown++)
+
+
 
     rightImageIndex = Math.floor(Math.random() * 20);
     var rightProductInstance = Product.arrOfProducts[rightImageIndex];
     rightProductElem.setAttribute('src', rightProductInstance.imageSrc)
     nameOfRightImage = rightProductInstance.name;
     rightProductInstance.timesShown++
-    // console.log('nameOfRightImage is ' + nameOfRightImage);
+
+    arrOfTimesShown.push(rightProductInstance.timesShown++)
+
+
 }
 pickThreeProducts();
 
@@ -280,19 +93,19 @@ pickThreeProducts();
 // console.log('nameOfLeftImage is ' + imageleft);
 
 var clickCounter = 0;
-var clickMaxDefault = 25;  //default max if no click count is inputted. 
-var clickMax = 0;
-var choice = 7;
+var clickMaxDefault =25;  //default max if no click count is inputted. 
+// var clickMax = 10;
+// var choice = 7;
 // var choice = window.prompt("Boss, How many clicks do you want the user to make ? If you enter nothing, your default click max is 25")
-if (choice.length == '0') {  //input is automatically a string in JS
-    clickMax = clickMaxDefault;
-    // console.log("click max " , clickMax)
+// if (choice.length == '0') {  //input is automatically a string in JS
+//     clickMax = clickMaxDefault;
+//     // console.log("click max " , clickMax)
 
-} else {
-    clickMax = choice;
-    // console.log("click max " , clickMax)
+// } else {
+//     clickMax = choice;
+//     // console.log("click max " , clickMax)
 
-}
+// }
 // console.log("")
 
 leftProductElem.addEventListener('click', clickHandler)
@@ -310,28 +123,12 @@ function terminateEvent() {
 
 function clickHandler(event) {
     // console.log('Product.arrOfProducts[leftImageIndex is',leftProduct);
-    // pickThreeProducts();
+    pickThreeProducts();
     // // for (var x =0; x <= 25; x++){
     //     var thingIclickedOn = event.target;
     var idOfProduct = event.target.id;
-   // console.log(idOfProduct)
-    //     var imageleft = nameOfLeftImage;
-    // var imagemiddle = nameOfMiddleImage;
-    // var imageright = nameOfRightImage;
-
-    // var img1 = document.getElementById(idOfProduct);
-   // console.log(img1)
-
-   // console.log("this is ", this);
-
-    // if(clickCounter == clickMax){
-    //     terminateEvent();
-    // }
-
-    // if(idOfProduct == 'left_product_img'){
+   console.log('arrtimeshown',arrOfTimesShown)
     
-
-    // }
     if (idOfProduct === 'left_product_img') {
         var leftProduct = Product.arrOfProducts[leftImageIndex];
         var leftName = leftProduct.name;
@@ -354,13 +151,48 @@ function clickHandler(event) {
         console.log("unnrecgonized click")
     }
     pickThreeProducts();
+    clickCounter++;
 
+    console.log('clickcounter is',clickCounter)
+    console.log('clickMaxDefault',clickMaxDefault)
+    
+    if(clickCounter == clickMaxDefault){
+        terminateEvent();
+    }
     
 }
 
-function renderPage(){
-    var tableHead = document.getElementById('th');
-    var tr = document.createElement('tr');
-    tableHead.appendChild(tr);
-    
+function myChart () {
+    var ctx = document.getElementById('myChart');
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tantaun', 'unicorn', 'usb', 'water-can', 'glass'],
+        datasets: [{
+            label: 'Number of Votes for Each Product',
+            data: [12, 19],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+ 
 }
+myChart ();
